1085f4a0b1be99c782aa7f4e19ab2b374154acdb
armanziaei <armanziaei83@gmaio.com>
2021-01-20 01:15:43 +0330

    Feat Presentor : MVP presentor created

--- app/src/main/java/com/example/mvp/MainActivity.java
+++ app/src/main/java/com/example/mvp/MainActivity.java
@@ -3,14 +3,18 @@ package com.example.mvp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.mvp.loginAction.view;
+
+public class MainActivity extends AppCompatActivity implements view {
 
     public EditText email, password;
-
+    MainActivityPresentor mainActivityPresentor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,15 +22,35 @@ public class MainActivity extends AppCompatActivity {
 
         email = (EditText) findViewById(R.id.email);
         password = (EditText) findViewById(R.id.password);
-
         Button loginBtn = (Button) findViewById(R.id.btn_login);
+
+        mainActivityPresentor = new MainActivityPresentor(this);
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                String getEmail = email.getText().toString();
+                String getPassword = password.getText().toString();
+
+                if (TextUtils.isEmpty(getEmail) || TextUtils.isEmpty(getPassword)){
+                    onError("Fill the empty feild(s)");
+                }else {
+                    onSuccess("Loged in, Welcome");
+                    mainActivityPresentor.checkInfo(getEmail , getPassword);
+                }
             }
         });
 
+    }
+
+    @Override
+    public void onError(String error) {
+        Toast.makeText(this , error , Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onSuccess(String message) {
+        Toast.makeText(this , message , Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/mvp/MainActivityPresentor.java
+++ app/src/main/java/com/example/mvp/MainActivityPresentor.java
@@ -1,5 +1,7 @@
 package com.example.mvp;
 
+import android.widget.Toast;
+
 public class MainActivityPresentor implements loginAction.presntor {
 
     loginAction.view view;
@@ -10,6 +12,10 @@ public class MainActivityPresentor implements loginAction.presntor {
 
     @Override
     public void checkInfo(String email, String password) {
-        
+        if (email.equals("arman@gmail.com") && password.equals("arman")){
+           view.onSuccess("Welocome Arman");
+        }else {
+            view.onError("Invaild Email or Password");
+        }
     }
 }
