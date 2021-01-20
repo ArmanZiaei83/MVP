ac832e8a57204b60347455536071b7cd3ceb526c
armanziaei <armanziaei83@gmaio.com>
2021-01-21 02:28:42 +0330

    Feat SignIn Activity : sign in activity created

---
+++ app/src/main/java/com/example/mvp/SingIn.java
@@ -0,0 +1,14 @@
+package com.example.mvp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SingIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sing_in);
+    }
+}
